af57d9d88cfc4b28df2f1ebe8bb9d5f1d730a64d
sin-doctor <sbs1602@naver.com>
2024-12-12 15:38:37 +0900

    직렬화 동기화

---
+++ JAVAPROJECT/src/com/kh/oopEx/Person.java
@@ -0,0 +1,22 @@
+package com.kh.oopEx;
+
+import java.io.Serializable;
+// implements Serializable 직렬화를 사용하기 위해 Serializable 경로 설정 후 객체파일 불러오기
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "유저정보{" + "name='" + name + '\'' + ", age=" + age + '}';
+    }
+}
--- JAVAPROJECT/src/com/kh/testEx/test3pre/StudentScoreManager.java
+++ JAVAPROJECT/src/com/kh/testEx/test3pre/StudentScoreManager.java
@@ -8,6 +8,7 @@ public class StudentScoreManager {
         // 학생이름과 점수를 저장할 Map 생성
         Map<String, Integer> studentScores = new HashMap<String, Integer>();
         StudentScoreManager ssm = new StudentScoreManager();
+
         /*
             학생 정보 저장
             학생 이름을 키(key), 점수를 값(value)으로 저장한다.
@@ -25,10 +26,12 @@ public class StudentScoreManager {
         저장된 모든 학생 이름과 점수를 출력한다.(모두 줄바꿈 형태)
         김철수: 85점 이영희: 92점 ...
         * */
-        System.out.println("학생 정보 출력");
-        for(Map.Entry<String, Integer> entry : studentScores.entrySet()) {
+        System.out.println("학생정보출력");
+        for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue() + "점");
         }
+
+
         /*
         점수 기준으로 합격/불합격 판단
         합격 기준은 80점 이상이다.
@@ -36,9 +39,8 @@ public class StudentScoreManager {
         형식 예시:(모두 줄바꿈 형태)
         김철수: 합격 이영희: 합격 박민수: 불합격
         */
-        System.out.println("합격 / 불합격 판단:");
+        System.out.println("합격/불합격 판단 : ");
         ssm.printPassFailStudentScores(studentScores);
-
 
         /*
         점수 수정 기능
@@ -48,33 +50,23 @@ public class StudentScoreManager {
         존재하지 않는 학생의 점수를 수정하려고 하면 존재하지 않는 학생입니다.라는 메시지를 출력한다.
         */
         String updateName = "박민수";
-        if(studentScores.containsKey(updateName)) {
+        if (studentScores.containsKey(updateName)) {
             studentScores.put(updateName, 82);
-            System.out.println(updateName + "의 점수를 82점으로 수정합니다.");
+            System.out.println(updateName + "의 점수를 82점으로 수정");
         } else {
-            System.out.println("존재하지 않는 학생입니다.");
+            System.out.println("존재하지 않은 학생입니다.");
         }
-
-        // 점수 수정 후 합격 / 불합격 판단
-
         System.out.println("점수 수정 후 합격 / 불합격 판단:");
         ssm.printPassFailStudentScores(studentScores);
 
 
-    }
+        // 점수 출력 기능 메서드
 
-    // 점수 출력 기능 메서드
+        /** 학생점수로 합격 불합격 판단여부 출력하는 메서드
+         * printPassFailStudentScores(Map<String, Integer> studentScores)
+         * @param studentScores = 특정학생들의 이름과 점수가 넘어오면 학생들의 대한 점수를 판단후 출력
+         */
 
-    /** 학생점수로 합격 불합격 판단여부 출력하는 메서드
-     * printPassFailStudentScores(Map<String, Integer> studentScores)
-     * @param studentScores = 특정학생들의 이름과 점수가 넘어오면 학생들의 대한 점수를 판단후 출력
-     */
-    public void printPassFailStudentScores(Map<String, Integer> studentScores) {
-        for(Map.Entry<String, Integer> entry : studentScores.entrySet()) {
-            String status = (entry.getValue()>=80) ?"합격" :"불합격";
-            System.out.println(entry.getKey() + " : " + status + " : " + entry.getValue());
-        }
-    }
 
 
 
@@ -92,4 +84,12 @@ public class StudentScoreManager {
     존재하지 않는 학생의 점수를 수정하려고 하면 존재하지 않는 학생입니다.라는 메시지를 출력한다.
 
      */
+    }
+
+    private void printPassFailStudentScores(Map<String, Integer> studentScores) {
+        for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
+            String status = (entry.getValue() >= 80) ? "합격" : "불합격";
+            System.out.println(entry.getKey() + " : " + status + " : " + entry.getValue());
+        }
+    }
 }
